54561a2a11ad4f872f037f4add05ec3e62b7ad62
manub <manu.bhardwaj_cs.da19@gla.ac.in>
2022-01-13 00:34:01 +0530

    creat a count methord using a tabular

--- src/Count_Starecase_tabulation.java
+++ src/Count_Starecase_tabulation.java
@@ -5,14 +5,16 @@ public class Count_Starecase_tabulation{
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         int arr[] = new int[num+1];
+        System.out.println("with memory =="+count_memory(num,arr));
+        System.out.println(count_tabular(num,arr));
 
     }
     static int count_memory(int n , int[] arr){
         if(n == 0)
-            return 0;
+            return 1;
         else if(n < 0)
             return 0;
-        if(arr[n]!=0)
+        if(arr[n]>0)
             return arr[n];
         int a = count_memory(n-1,arr);
         int b = count_memory(n-2,arr);
@@ -20,5 +22,19 @@ public class Count_Starecase_tabulation{
         int sum = a + b + c;
         arr[n] = sum;
         return sum;
+    }
+    static int count_tabular(int n,int[] arr){
+
+        arr[0] =1;
+        for (int i = 1; i < n; i++) {
+            if (n == 1)
+               arr[i] = arr[n - 1];
+            else if (n == 2)
+                arr[i] = arr[n - 1] + arr[n - 2];
+            else
+                arr[i] = arr[n-1]+arr[n-2]+arr[n-3] ;
+
+        }
+        return arr[n];
     }
 }
