94b51937662e94c4021bff8535878b1d13e449cb
manub <manu.bhardwaj_cs.da19@gla.ac.in>
2022-01-13 00:23:57 +0530

    creat a count methord using a memory

--- src/Count_Starecase_tabulation.java
+++ src/Count_Starecase_tabulation.java
@@ -1,2 +1,24 @@
-public class Count_Starecase_tabulation {
+import java.util.Scanner;
+
+public class Count_Starecase_tabulation{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int arr[] = new int[num+1];
+
+    }
+    static int count_memory(int n , int[] arr){
+        if(n == 0)
+            return 0;
+        else if(n < 0)
+            return 0;
+        if(arr[n]!=0)
+            return arr[n];
+        int a = count_memory(n-1,arr);
+        int b = count_memory(n-2,arr);
+        int c = count_memory(n-3,arr);
+        int sum = a + b + c;
+        arr[n] = sum;
+        return sum;
+    }
 }
