63eed4338c075d5ab95a2967d648d231d3d43be6
manub <manu.bhardwaj_cs.da19@gla.ac.in>
2022-01-12 23:42:16 +0530

    creat a fabo function to have ha recursive code for fabocani

--- src/fabonaci_series_dp.java
+++ src/fabonaci_series_dp.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class fabonaci_series_dp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number for fabocani");
+        int number = sc.nextInt();
+        int arr[] = new int[number+1];
+        System.out.println(fabo(number,arr));
+    }
+    static int fabo(int n,int[] arr){
+     return 0;
+    }
 }
