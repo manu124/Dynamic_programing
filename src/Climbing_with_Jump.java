a5974707b8e32888edded2e5b99e2bb97fd722bd
manub <manu.bhardwaj_cs.da19@gla.ac.in>
2022-01-14 00:20:28 +0530

    complete code with all function done

--- src/Climbing_with_Jump.java
+++ src/Climbing_with_Jump.java
@@ -10,6 +10,16 @@ public class Climbing_with_Jump {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+        System.out.println("output ="+count(n,dp,arr));
 
+    }
+    static int count(int n,int[] dp,int[] arr){
+        dp[n]=1;
+        for (int i = n-1; i >=0 ; i--) {
+            for (int j = 0; j <= arr[i] && (i+j<dp.length); j++) {
+                dp[i] += dp[i+j];
+            }
+        }
+        return dp[0];
     }
 }
