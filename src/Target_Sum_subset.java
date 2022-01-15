86def5f60517ccc9e2d5ec146b7cd2d1c4c4049f
manub <manu.bhardwaj_cs.da19@gla.ac.in>
2022-01-15 13:08:54 +0530

    target sum subset

--- src/Target_Sum_subset.java
+++ src/Target_Sum_subset.java
@@ -3,6 +3,36 @@ import java.util.Scanner;
 public class Target_Sum_subset {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter a array size");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = sc.nextInt();
 
+        int tar = sc.nextInt();
+        boolean[][] dp = new boolean[size + 1][tar + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0) {
+                    dp[i][j] = false;
+                } else if (j == 0) {
+                    dp[i][j] = true;
+                } else {//we check immidate above
+                    if (dp[i - 1][j] == true) {
+                        dp[i][j] = true;
+                    } else {
+                        int val = arr[i - 1];
+                        if (j >= val) {
+                            if (dp[i - 1][j - val] == true)
+                                dp[i][j] = true;
+                        }
+                    }
+                }
+            }
+
+        }
+        System.out.println(dp[arr.length][tar]);
     }
 }
